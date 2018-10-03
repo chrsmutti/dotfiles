df6d41f53d73d0d7a7d8cb13cae9af7a0d18ff32
Christian Mutti <chrsmutti@gmail.com>
2018-10-03 13:47:09 -0300

    Added nvim autoload and some minor changes

--- setup.fish
+++ setup.fish
@@ -1,6 +1,12 @@
 #!/usr/bin/env fish
 set DIR (cd (dirname (status -f)); and pwd) 
 
+# mkdirs
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/fish
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.local/share/nvim/site
+
 # delete files
 rm -f ~/.config/nvim/init.vim
 rm -f ~/.config/fish/config.fish
@@ -11,4 +17,5 @@ rm -f ~/.tmux.conf
 ln -s $DIR/nvim/init.vim ~/.config/nvim/init.vim
 ln -s $DIR/fish/cinfig.fish ~/.config/fish/config.fish
 ln -s $DIR/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -s $DIR/.tmux.conf ~/.tmux.conf
+ln -s $DIR/tmux.conf ~/.tmux.conf
+ln -s $DIR/nvim/site/autoload ~/.local/share/nvim/site/autoload
