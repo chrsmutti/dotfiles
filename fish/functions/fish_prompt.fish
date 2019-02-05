0efc2e8cda802182b4a1fdc5d4f66156bb1f8937
Christian Mutti <chrsmutti@gmail.com>
2019-02-05 13:10:37 -0200

    Some changes to prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,4 +1,8 @@
+# Defined in fish_prompt.fish @ line 1
 function fish_prompt
+	set -l last_status $status
+
+	
 	set_color --bold white
 	echo -n (whoami)
 
@@ -6,22 +10,38 @@ function fish_prompt
 	echo -n '@'
 	echo -n (hostname)" "
 
-	set_color --bold red
+	set_color --bold green
 	echo -n (prompt_pwd)
 
-	set_color white
-	printf '%s ' (__fish_git_prompt)
+	if git_is_repo
+		set_color --bold white
+		echo -n " ("	
+		if git_is_dirty; or git_is_touched
+			set_color --bold red
+		else if git_is_staged
+			set_color --bold green
+		end
+	
+		echo -n (git_branch_name)(git_ahead)
+		set_color --bold white
+		echo -n ") "
+	end
 
 	if test -e package.json
 		set_color --bold green
 		echo -n "$mfizz_nodejs ("(node -v)") "
 	end
+	
+	if not test $last_status -eq 0 
+		set_color $fish_color_error
+		echo -n "[$last_status]"
+	end
 
 	set_color --bold red
 	if [ (whoami) = "root" ]
-		echo -n '# '
+		printf "\n%s " (echo -n "#")
 	else
-		echo -n " $firacode_equal_greater "
+		printf "\n%s " (echo -n " $firacode_equal_greater")
 	end
   	set_color normal
 end
