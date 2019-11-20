dc5aba27821ed558aff572e26ad735f417acb8e4
Christian Mutti <christian.mutti@vtex.com.br>
2019-11-19 21:02:19 -0300

    Added hbase aliases

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,6 @@
 if not functions -q fisher
-    curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-    fish -c fisher
+  curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+  fish -c fisher
 end
 
 # no greeting
@@ -14,13 +14,14 @@ set -gx GOPATH $HOME/Workspace/go
 set -gx GOBIN $GOPATH/bin
 set -gx GOSRC $GOPATH/src
 
+set PATH $PATH /usr/local/bin
 set PATH $PATH $HOME/.cargo/bin
 set PATH $PATH $HOME/.local/bin
 set PATH $PATH $HOME/.yarn/bin
 set PATH $PATH $HOME/.rvm/bin
 set PATH $PATH $GOBIN
-set PATH $PATH /usr/local/bin
 
+# spacefish variables
 set SPACEFISH_EXEC_TIME_SHOW false
 set SPACEFISH_PROMPT_ADD_NEWLINE false
 set SPACEFISH_PROMPT_SUFFIXES_SHOW true
@@ -35,21 +36,26 @@ alias ssh "env TERM=xterm-256color ssh"
 alias sbt "env TERM=xterm-color sbt"
 alias scala "env TERM=xterm-color scala -Dscala.color"
 
+if [ -e ~/.local/share/hbase/bin/hbase ]
+  alias start-hbase "~/.local/share/hbase/bin/start-hbase.sh"
+  alias hbase "~/.local/share/hbase/bin/hbase"
+end
+
 if [ -e ~/.cargo/bin/exa ]
-    alias l 'exa'
-    alias ls 'exa'
-    alias ll 'exa -l'
-    alias lll 'exa -la'
+  alias l 'exa'
+  alias ls 'exa'
+  alias ll 'exa -l'
+  alias lll 'exa -la'
 else
-    alias l 'ls'
-    alias ll 'ls -l'
-    alias lll 'ls -la'
+  alias l 'ls'
+  alias ll 'ls -l'
+  alias lll 'ls -la'
 end
 
 if [ -e /usr/local/bin/bat ]
-    alias cat bat
+  alias cat bat
 end
 
 if [ -e /usr/local/bin/fnm ]
-    fnm env --multi | source
+  fnm env --multi | source
 end
