38ce90ad6525ea2c3f452357684067e03b323153
Christian Mutti <chrsmutti@gmail.com>
2021-10-04 21:29:58 -0300

    Check for Darwin on brewery, add alacritty.yml

--- scripts/brewery.sh
+++ scripts/brewery.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 # A script used to install some much needed functionality using brew.
+
+if [[ "$(uname)" != "Darwin"* ]]; then
+    echo "This script is only for macos"
+    exit 0
+fi
+
 brew install starship
 brew install git-delta
