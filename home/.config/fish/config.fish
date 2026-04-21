98f16f3cd578ef450ccef27eddff8f7b7459ebb3
Christian Mutti <christian@chrs.dev>
2026-04-21 16:43:14 +0100

    Simplify configs

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,50 +1,28 @@
 function fish_greeting
 end
 
-fish_add_path "$HOME/.cargo/bin"
+if status is-interactive
+    source "$HOME/.cargo/env.fish"
 
-# fnm (should be installed via cargo)
-fnm env | source
+    fish_add_path "$HOME/.yarn/bin"
+    fish_add_path "$HOME/.local/bin"
 
-# yarn
-fish_add_path "$HOME/.yarn/bin"
-fish_add_path "$HOME/.local/bin"
+    set -gx GOPATH "$HOME/go"
+    fish_add_path "$GOPATH/bin"
 
-# go
-set -gx GOPATH "$HOME/go"
-fish_add_path "$GOPATH/bin"
+    fish_add_path "/opt/homebrew/bin"
 
-# homebrew
-fish_add_path "/opt/homebrew/bin"
+    set -gx SDKMAN_DIR "$HOME/.sdkman"
 
-# pyenv
-set -gx PYENV_ROOT "$HOME/.pyenv"
-if test -e "$PYENV_ROOT/bin"
-    fish_add_path "$PYENV_ROOT/bin"
-    pyenv init - | source
+    zoxide init fish --cmd=cd | source
+
+    alias ls "exa"
+    alias ll "exa -l"
+    alias cat "bat"
+    alias fish_reload "source ~/.config/fish/config.fish"
+
+    alias gaa "git add --all; git commit -a"
+    alias gc "git checkout"
+
+	fnm env --use-on-cd --shell fish | source
 end
-
-# sdkman
-set -gx SDKMAN_DIR "$HOME/.sdkman"
-
-# nvim
-set -gx EDITOR "nvim"
-alias vim "nvim"
-
-# zoxide
-zoxide init fish --cmd=cd | source
-
-# ocaml
-eval (opam env)
-
-# aliases
-alias ls "exa"
-alias ll "exa -l"
-alias cat "bat"
-alias fish_reload "source ~/.config/fish/config.fish"
-
-alias gaa "git add --all; git commit -a"
-alias gc "git checkout"
-
-starship init fish | source
-
